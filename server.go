b2cf3c2b8e7efbc5a67a52404959c2a40f4e74d3
Michael Rybka <mikerybka@users.noreply.github.com>
2025-02-21 11:35:55 -0500

    Update server.go

--- server.go
+++ server.go
@@ -74,7 +74,7 @@ type LoginRequest struct {
 
 type LoginResponse struct {
 	Token   string   `json:"token"`
-	UserIDs []string `json:"user_ids"`
+	UserIDs []string `json:"userIDs"`
 	Error   error    `json:"error"`
 }
 
